fa3a1f9b5ba5676dee824b0318b70ac65c90dd5f
realadirao <148047260+realadirao@users.noreply.github.com>
2025-07-09 13:31:03 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chatbot from "./Chatbot";
+import Dashboard from "./Dashboard";
 import "./App.css";
 
-function App() {
+// A simple component to represent the main page with the chatbot
+function ChatbotPage() {
   return (
     <div className="App">
       <Chatbot />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ChatbotPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
